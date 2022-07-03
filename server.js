7cced230d2f4ba53632760281ca14be727ab772b
Walid Ahmed <walidsayed9811@gmail.com>
2022-07-03 22:32:48 +0200

    Revert "Revert "Revert "Api_for_e-commerce"""
    This reverts commit bb0d7a8df12f426f825cbb7a19020ded295dd5a0.

--- server.js
+++ server.js
@@ -1,56 +1,21 @@
 const express = require("express");
 const app = express();
-app.use(express.json())
-const morgan = require("morgan");
-const connectDB = require('./backend/config/db')
-const dotenv = require('dotenv')
-const util = require('util')
-const path = require('path')
-const fileUpload = require('express-fileupload')
+ const connectDB = require("./backend/config/db")
+ const dotenv = require("dotenv")
 
-const userRoutes = require("./backend/routes/userRoutes");
-// const orderRoutes = require("./api/routes/orders");
+const routes = require("./backend/routes/routes");
+app.use(express.json());
 
-dotenv.config()
+ dotenv.config()
 
-connectDB()
+ connectDB()
 
-// app.use('/uploads', express.static('uploads'));
-app.use(express.json())
-app.use(express.urlencoded({extended: true}));
-app.use(fileUpload())
+// Middleware || all our endpoints will start from '/api'.
+app.use("/api", routes);
 
-  
-// Routes which should handle requests
-app.use("/users", userRoutes);
-// app.use("/orders", orderRoutes);
 
-app.use((req, res, next) => {
-  if(req.file) {
-    // return res.status(201).json({req.body});
-    res.status(201).json({
-      message: "Created user successfully",
-      })
-      next()
-  }
-  const error = new Error("Not userdata found");
-  error.status = 404;
-  next(error);
+app.listen(3000, ()=>{
+  console.log('Server Running')
 });
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message
-    }
-  });
-});
-
-const http = require('http');
-const server = http.createServer(app);
-
-server.listen(3000, () => console.log('Server Running on port 3000'));
-
 
 
